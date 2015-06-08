e1a0b35031421d75450906fdcee11ef65d821884
justincolangelo <justincolangelo@gmail.com>
2015-06-08 14:03:01 -0400

    Adding web base models

---
+++ DirigoEdge/Models/Base/WebBaseAdminModel.cs
@@ -0,0 +1,17 @@
+﻿using DirigoEdge.Data.Context;
+using DirigoEdgeCore.Models;
+using DirigoEdgeCore.Utils.Logging;
+
+namespace DirigoEdge.Models.Base
+{
+    public class WebBaseAdminModel : DirigoBaseModel
+    {
+        protected new ILog Log = LogFactory.GetLog(typeof(WebBaseAdminModel));
+
+        private WebDataContext _context;
+        protected new WebDataContext Context
+        {
+            get { return _context ?? (_context = new WebDataContext()); }
+        }
+    }
+}
---
+++ DirigoEdge/Models/Base/WebBaseModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using DirigoEdge.Data.Context;
+using DirigoEdge.Utils;
+using DirigoEdge.Utils.Logging;
+using DirigoEdgeCore.Data.Context;
+using DirigoEdgeCore.Models;
+using DirigoEdgeCore.Utils.Logging;
+
+namespace DirigoEdge.Models.Base
+{
+    public class WebBaseModel : DirigoBaseModel
+    {
+        protected new ILog Log = LogFactory.GetLog(typeof(WebBaseModel));
+
+        private WebDataContext _context;
+        protected new WebDataContext Context
+        {
+            get { return _context ?? (_context = new WebDataContext()); }
+        }
+    }
+}
