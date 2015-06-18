a49c73b8109b1dcd20e8653ad6963e9ed054e4b7
Derek Wheelden <derek.wheelden@gmail.com>
2015-06-18 17:45:05 -0400

    ContentController can find nth-level pages now. Fixes #106.

--- DirigoEdge/Controllers/ContentController.cs
+++ DirigoEdge/Controllers/ContentController.cs
@@ -129,7 +129,7 @@ namespace DirigoEdge.Controllers
 
         private ContentViewViewModel GetSubDirectoryModel(string path)
         {
-            var masterList = CachedObjects.GetCacheNavigationList();
+            var masterList = CachedObjects.GetCacheNavigationList(1);
             var pathPieces = path.Split('/');
             var permalink = pathPieces.Last().ToLower();
 
