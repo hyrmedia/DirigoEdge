94891db8ece355f3d9755b2c7c03afd7a876147d
Chris Belanger <belanger.chris@gmail.com>
2015-08-26 16:39:43 -0400

    tests:

---
+++ SmokeTests/HomePageTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SmokeTests
+{
+    [TestFixture]
+    public class HomePageTests : SmokeTestScaffolds
+    {
+        [TestFixtureSetUp]
+        public void FixtureSetup()
+        {
+            Driver = new ChromeDriver();
+            Driver.Manage().Timeouts().ImplicitlyWait(new TimeSpan(0, 0, 30));
+        }
+
+        [Test]
+        public void LoadHomePage()
+        {
+            var response = GetResponseFromUrl(BaseUrl);
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
+        }
+
+        [Test]
+        public void ValidateHomePage()
+        {
+            Driver.Navigate().GoToUrl(BaseUrl);
+            Driver.FindElement(By.CssSelector(".contentPage"));
+        }
+
+        [Test]
+        public void LoadBlogListingPage()
+        {
+            var response = GetResponseFromUrl(BaseUrl + "blog");
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
+        }
+
+        [Test]
+        public void ValidateBlogListing()
+        {
+            Driver.Navigate().GoToUrl(BaseUrl + "blog");
+            Driver.FindElement(By.CssSelector(".blogListing"));
+        }
+    }
+}
---
+++ SmokeTests/SmokeTestScaffolds.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace SmokeTests
+{
+    [TestFixture]
+    public class SmokeTestScaffolds
+    {
+        protected IWebDriver Driver;
+        protected const String BaseUrl = "http://tfs2013/";
+
+        protected static HttpWebResponse GetResponseFromUrl(String url)
+        {
+            var webRequest = (HttpWebRequest)WebRequest.Create(url);
+            var response = (HttpWebResponse)webRequest.GetResponse();
+            return response;
+        }
+
+        [TestFixtureTearDown]
+        public void TearDown()
+        {
+            Driver?.Quit();
+        }
+    }
+}
