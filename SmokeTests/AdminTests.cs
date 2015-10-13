2ef826682183f1bebf5b6e68e047880f4a8f81dc
Chris Belanger <belanger.chris@gmail.com>
2015-10-13 06:48:28 -0400

    }

---
+++ SmokeTests/AdminTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SmokeTests
+{
+    [TestFixture]
+    public class AdminTests : SmokeTestScaffolds
+    {
+        [TestFixtureSetUp]
+        public void FixtureSetup()
+        {
+            Driver = new ChromeDriver();
+            Driver.Manage().Timeouts().ImplicitlyWait(new TimeSpan(0, 0, 30));
+        }
+
+        [Test]
+        public void Login()
+        {
+            Driver.Navigate().GoToUrl(BaseUrl + "/account/login/");
+
+            var usernameField = Driver.FindElement(By.Id("UserName"));
+            var passwordField = Driver.FindElement(By.Id("Password"));
+            var submitButton = Driver.FindElement(By.ClassName("btn-default"));
+
+            Assert.IsNotNull(usernameField);
+            Assert.IsNotNull(passwordField);
+
+            usernameField.SendKeys("cbelanger");
+            passwordField.SendKeys("Password1!");
+
+            submitButton.Click();
+            Thread.Sleep(2000);
+            Driver.Navigate().GoToUrl(BaseUrl + "admin/");
+            Assert.AreEqual("Edge Dashboard", Driver.Title);
+        }
+    }
+}
