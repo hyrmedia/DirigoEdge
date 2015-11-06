ee4277328599a1acc9399bf84f41e16b59a38b7f
Chris Belanger <belanger.chris@gmail.com>
2015-11-06 13:59:01 -0500

    file

---
+++ SmokeTests/TestingTools.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace SmokeTests
+{
+    public class TestingTools
+    {
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijxlmnopqrstuvwxyz 0123456789";
+            var random = new Random();
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
